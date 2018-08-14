b23952ef44f08eedbd4d1b1ccfafc1d146de5350
davidjaggi <davidjaggi@users.noreply.github.com>
2018-08-14 21:50:47 +0100

    Added new problem ser

---
+++ MITx_6.00.1x/PS_2.2.py
@@ -0,0 +1,32 @@
+# Test Case 1:
+# balance = 3329
+# annualInterestRate = 0.2
+
+
+# Test Case 2:
+balance = 4773
+annualInterestRate = 0.2
+
+# Test Case 3:
+# balance = 3926
+# annualInterestRate = 0.2
+
+monthlyInterestRate = annualInterestRate / 12.0
+initialBalance = balance
+month = 0
+minimumPayment = 0
+
+def calculator(month,balance,minimumPayment,monthlyInterestRate):
+    while month < 12:
+        unpaidBalance = balance - minimumPayment
+        balance = unpaidBalance + (monthlyInterestRate*unpaidBalance)
+        month += 1
+    return balance
+
+while calculator(month,balance,minimumPayment,monthlyInterestRate) > 0:
+    balance = initialBalance
+    minimumPayment += 10
+    month = 0
+    calculator(month,balance,minimumPayment,monthlyInterestRate)
+
+print(f'Lowest Payment: {minimumPayment}')
