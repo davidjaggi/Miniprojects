f6afd0975321bc5988551deeb5f3aeca76f36575
davidjaggi <davidjaggi@users.noreply.github.com>
2018-08-14 21:38:56 +0100

    Added Files

---
+++ MITx_6.00.1x/PS_2.1.py
@@ -0,0 +1,23 @@
+# Test Case 1 
+# balance = 42
+# annualInterestRate = 0.2
+# monthlyPaymentRate = 0.04
+
+# Test Case 2
+# balance = 484
+# annualInterestRate = 0.2
+# monthlyPaymentRate = 0.04
+
+
+month = 0
+totalPayment = 0
+monthlyInterestRate = annualInterestRate / 12.0
+
+while month < 12:
+    minimumMonthlyPayment = monthlyPaymentRate * balance
+    unpaidBalance = balance - minimumMonthlyPayment
+    totalPayment += minimumMonthlyPayment
+    balance = unpaidBalance + (monthlyInterestRate * unpaidBalance)
+    month += 1
+
+print(f'Remaining balance: {round(balance,2)}')
